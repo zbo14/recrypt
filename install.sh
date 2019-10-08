cb3669384ebdddb1f2f0fc852bc6d6e815896c62
zbo14 <zbalder@starry.com>
2019-10-08 16:59:24 -0400

    simplify install, fix path in recrypt script

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@
 apt update
 apt install -y gnupg zip
 
-chmod u+x scripts/decrypt.sh
-chmod u+x scripts/encrypt.sh
-chmod u+x scripts/recrypt.sh
+chmod u+x scripts/{decrypt,encrypt,recrypt}
 
-cp scripts/decrypt.sh /usr/bin/decrypt
-cp scripts/encrypt.sh /usr/bin/encrypt
-cp scripts/recrypt.sh /usr/bin/recrypt
+cp -R scripts /usr/bin
